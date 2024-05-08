f3c49afd25f5658ed355fee4164d836d68ee55a7
manisasani <manisasani2005@gmail.com>
2024-05-08 23:04:00 +0430

    add pagination to book list view

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from .models import Book
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
     
